47c1b607d19cefcb9bc9f4d477454f469af2de75
cl1ents <gh@cli.lol>
2026-02-07 11:16:31 +0100

    Fix nix shell env

--- shell.nix
+++ shell.nix
@@ -7,12 +7,17 @@ pkgs.callPackage (
     mkShell,
     cargo,
     rustc,
+    clippy,
+    rustfmt,
   }:
   mkShell {
     strictDeps = true;
     nativeBuildInputs = [
       cargo
       rustc
+      clippy
+      rustfmt
     ];
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   }
 ) { }
